6c10a8529d7513ef0d0296a389983dd369762d0b
mohamed ali <60578550+ali635@users.noreply.github.com>
2021-09-15 19:37:24 +0200

    Merage Repo

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -16,6 +16,36 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(
             'App\Http\Interfaces\AdminRepositoryInterface',
             'App\Http\Repository\AdminRepository');
+        
+        $this->app->bind(
+            'App\Http\Interfaces\AdminAuthRepositoryInterface',
+            'App\Http\Repository\AdminAuthRepository');
+
+        $this->app->bind(
+            'App\Http\Interfaces\ReceiptRepositoryInterface',
+            'App\Http\Repository\ReceiptRepository');
+
+        $this->app->bind(
+            'App\Http\Interfaces\FeeRepositoryInterface',
+            'App\Http\Repository\FeeRepository');
+
+        $this->app->bind(
+            'App\Http\Interfaces\StoreRepositoryInterface',
+            'App\Http\Repository\StoreRepository');
+
+        $this->app->bind(
+            'App\Http\Interfaces\ProductRepositoryInterface',
+            'App\Http\Repository\ProductRepository');
+
+
+        $this->app->bind(
+            'App\Http\Interfaces\TaxRepositoryInterface',
+            'App\Http\Repository\TaxRepository');
+
+        $this->app->bind(
+            'App\Http\Interfaces\RoleRepositoryInterface',
+            'App\Http\Repository\RoleRepository');
+        
 
         $this->app->bind(
                 'App\Http\Interfaces\UserInterface',
