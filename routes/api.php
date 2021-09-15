939172bb9e04a217c43924effe222deeeaa1871e
mohamed ali <60578550+ali635@users.noreply.github.com>
2021-09-15 19:35:12 +0200

    Merge Api

--- routes/api.php
+++ routes/api.php
@@ -22,14 +22,21 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
-/*Route::group(['prefix' => 'admin'],function (){
-    Route::post('login', 'API\AdminController@index');
-    // you masy login in system
+
+Route::group(['prefix' => 'admin'],function (){
+    Route::post('login', 'API\AdminAuthController@index');
+    // you masy login in system 
     Route::middleware('auth:admin-api')->group(function(){
-        Route::post('logout','API\AdminController@logout');
+        Route::post('logout','API\AdminAuthController@logout');
+        Route::apiResource('Receipt','API\ReceiptController')->middleware('can:receipts');
+        Route::apiResource('Fee','API\FeeController')->middleware('can:fees');
+        Route::apiResource('Admin','API\AdminController')->middleware('can:admins');
+        Route::apiResource('Store','API\StoreController')->middleware('can:stores');
+        Route::apiResource('Product','API\ProductController')->middleware('can:products');
+        Route::apiResource('Tax','API\TaxController')->middleware('can:taxes');
+        Route::apiResource('Role','API\RoleController')->middleware('can:roles');
     });
 });
-*/
 
 Route::apiResource('user','API\UserController');
 Route::apiResource('sells','API\SellsController');
